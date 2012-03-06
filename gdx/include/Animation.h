0703db7d6ef22c8e05a4264f17b999dee5333f15
pcman <pcman75@me.com>
2012-03-06 19:09:01 +0200

    Animation

---
+++ gdx/include/Animation.h
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#pragma once
+
+#include "TextureRegion.h"
+
+/** <p>
+ * An Animation stores a list of {@link TextureRegion}s representing an animated sequence, e.g. for running or jumping. Each
+ * region of an Animation is called a key frame, multiple key frames make up the animation.
+ * <p>
+ */
+class Animation
+{
+private:
+	TextureRegion** m_keyFrames;
+	int m_keyFramesNum;
+
+public:
+	float frameDuration;
+
+public:
+	/** Constructor, storing the frame duration and key frames.
+	 *
+	 * @param frameDuration the time between frames in seconds.
+	 * @param keyFrames the {@link TextureRegion}s representing the frames. */
+	Animation(float frameDuration,const std::list<TextureRegion*>& keyFrames);
+
+	/** Returns a {@link TextureRegion} based on the so called state time. This is the amount of seconds an object has spent in the
+	 * state this Animation instance represents, e.g. running, jumping and so on. The mode specifies whether the animation is
+	 * looping or not.
+	 * @param stateTime the time spent in the state represented by this animation.
+	 * @param looping whether the animation is looping or not.
+	 * @return the TextureRegion representing the frame of animation for the given state time. */
+	TextureRegion* getKeyFrame(float stateTime, bool looping);
+
+	/**
+	 * Whether the animation would be finished if played without looping
+	 * given the state time.
+	 * @param stateTime
+	 * @return whether the animation is finished.
+	 */
+	bool isAnimationFinished(float stateTime);
+};
---
+++ gdx/src/Animation.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "Animation.h"
+
+Animation::Animation(float frameDuration,const std::list<TextureRegion*>& keyFrames)
+{
+	m_keyFramesNum = keyFrames.size();
+	this->frameDuration = frameDuration;
+	m_keyFrames = new TextureRegion*[keyFrames.size()];
+
+	int i = 0;
+	for(std::list<TextureRegion*>::const_iterator it = keyFrames.begin(); it != keyFrames.end(); it++)
+	{
+		m_keyFrames[i] = *it;
+		i++;
+	}
+}
+
+
+/** Returns a {@link TextureRegion} based on the so called state time. This is the amount of seconds an object has spent in the
+* state this Animation instance represents, e.g. running, jumping and so on. The mode specifies whether the animation is
+* looping or not.
+* @param stateTime the time spent in the state represented by this animation.
+* @param looping whether the animation is looping or not.
+* @return the TextureRegion representing the frame of animation for the given state time. */
+TextureRegion* Animation::getKeyFrame(float stateTime, bool looping)
+{
+	int frameNumber = (int)(stateTime / frameDuration);
+
+	if(!looping)
+	{
+		frameNumber = min(m_keyFramesNum - 1, frameNumber);
+	}
+	else
+	{
+		frameNumber = frameNumber % m_keyFramesNum;
+	}
+	return m_keyFrames[frameNumber];
+}
+
+/**
+* Whether the animation would be finished if played without looping
+* given the state time.
+* @param stateTime
+* @return whether the animation is finished.
+*/
+bool Animation::isAnimationFinished(float stateTime)
+{
+	int frameNumber = (int)(stateTime / frameDuration);
+	return m_keyFramesNum - 1 < frameNumber;
+}
+
