6a4a0e220e1d041c9385f513644276147905c526
pcman <pcman75@me.com>
2012-03-12 17:43:01 +0200

    start Invaders

---
+++ demos/GdxInvaders/src/GdxInvadersDesktop.cpp
@@ -0,0 +1,16 @@
+// GdxInvaders.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "GdxInvadersDesktop.h"
+#include "WoglApplication.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+    //WoglApplication(GdxInvaders(), "Gdx Invaders", 800, 480, false);
+    return 0;
+}
+
