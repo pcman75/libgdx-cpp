dc2b4b755559449c3df6649537b8abc47a47fcc1
pcman <pcman75@me.com>
2012-04-02 01:24:32 +0300

    osx build

---
+++ backends/backend-glfw/exp/PosixFiles.cpp
@@ -0,0 +1,9 @@
+//
+//  PosixFile.cpp
+//  backend-glfw
+//
+//  Created by Cosmin Manoliu on 4/1/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#include <iostream>
---
+++ backends/backend-glfw/exp/PosixFiles.h
@@ -0,0 +1,14 @@
+//
+//  PosixFile.h
+//  backend-glfw
+//
+//  Created by Cosmin Manoliu on 4/1/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#ifndef backend_glfw_PosixFile_h
+#define backend_glfw_PosixFile_h
+
+
+
+#endif
