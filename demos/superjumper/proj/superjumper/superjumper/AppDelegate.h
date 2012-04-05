0a7f1c953fdda92d560ffa24ed3e624665b0bf88
pcman <pcman75@me.com>
2012-04-06 00:52:34 +0300

    superjumper OSX

---
+++ demos/superjumper/proj/superjumper/superjumper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  superjumper
+//
+//  Created by Cosmin Manoliu on 4/5/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ demos/superjumper/proj/superjumper/superjumper/AppDelegate.mm
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  superjumper
+//
+//  Created by Cosmin Manoliu on 4/5/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
