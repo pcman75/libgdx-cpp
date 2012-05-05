72bf871316e494d4ff27ba3b433b5ddc3ba58899
pcman <pcman75@me.com>
2012-05-06 00:13:39 +0300

    repair accidental delete at previous commit

---
+++ tests/gdx-tests-ios/gdx-tests-ios/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  gdx-backend-ios
+//
+//  Created by Tamas Jano on 29/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GLView.h"
+#import "IOSApplication.h"
+
+//@class RootViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    UIWindow*                   m_window;
+    GLView*                     m_view;
+    IOSApplication*             m_app;
+    
+}
+
+@property (strong, nonatomic) UIWindow *m_window;
+//@property (nonatomic, retain) RootViewController *rootViewController;
+
+-(AppDelegate*) initWithApplication:(IOSApplication*)application;
+
+@end
